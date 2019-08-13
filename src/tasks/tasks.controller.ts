ac8ebff47aa33665dc99461bbd9346f222d89831
Arley HR <arley.hr@hotmail.com>
2019-08-13 10:54:06 -0500

    receiving post params

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -12,7 +12,10 @@ export class TasksController {
     }
 
     @Post()
-    createTask(): Task {
-        return this.taskService.createTask('hi', 'node');
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task {
+        return this.taskService.createTask(title, description);
     }
 }
