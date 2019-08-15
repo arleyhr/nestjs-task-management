27cd8e8a506091cb12cc4de866fc21b40b8722a9
Arley HR <arley.hr@hotmail.com>
2019-08-15 11:47:44 -0500

    typeorm config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 54320,
+    username: 'root',
+    password: '123456',
+    database: 'nestjs_task_management',
+    entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+    synchronize: true, // note: disable in production
+};
+
+export default typeOrmConfig;
